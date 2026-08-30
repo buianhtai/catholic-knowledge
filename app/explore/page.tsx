2a1d18e8db209114bd3cdcccc0e1f8433cc69b54
Tai.Bui <buianhtai90@gmail.com>
2026-08-30 18:01:56 +0700

    refactor: remove explore responsive wrapper

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,10 +1,5 @@
 import KnowledgeGraphExplorer from '@/components/graph/KnowledgeGraphExplorer';
-import responsive from '@/components/graph/KnowledgeGraphResponsive.module.css';
 
 export default function ExplorePage() {
-  return (
-    <div className={responsive.page}>
-      <KnowledgeGraphExplorer />
-    </div>
-  );
+  return <KnowledgeGraphExplorer />;
 }
