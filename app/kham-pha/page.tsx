732b9913bec9873f495ab180eab8dcf817d50f53
Tai.Bui <buianhtai90@gmail.com>
2026-08-30 17:26:10 +0700

    refactor: reuse shared explore responsive layout

--- app/kham-pha/page.tsx
+++ app/kham-pha/page.tsx
@@ -1,10 +1,1 @@
-import ExplorePage from '@/app/explore/page';
-import styles from './KhamPhaResponsive.module.css';
-
-export default function KhamPhaPage() {
-  return (
-    <div className={styles.page}>
-      <ExplorePage />
-    </div>
-  );
-}
+export { default } from '@/app/explore/page';
